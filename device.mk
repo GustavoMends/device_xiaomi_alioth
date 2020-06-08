9c1087a22dea4ba9a9af0cd7cd6611bed67553ef
Jackeagle <jackeagle102@gmail.com>
2020-06-08 19:22:04 +0200

    alioth: Build libvndfwk_detect_jni.qti.vendor
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I45a11169c0382db0305ac0639903df03385ec38b

--- device.mk
+++ device.mk
@@ -290,6 +290,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom
