f91af5dc57278b2f6f83d678125a4e7de7906dc0
Gustavo Mendes <gusttavo.me@outlook.com>
2022-09-10 11:58:37 -0300

    alioth: Inherit Google Camera * https://gitlab.com/GustavoMends/vendor_GoogleCamera Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -199,6 +199,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Inherit Google Camera
+$(call inherit-product, vendor/GoogleCamera/config.mk)
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
