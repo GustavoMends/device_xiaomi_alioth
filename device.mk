70cf728bce8baae7e4e8db42fb96dc0916ad397d
EmanuelCN <emanuelghub@gmail.com>
2022-12-26 18:32:46 +0000

    alioth: rootdir: Cleanup
    Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Change-Id: I1d7b5a532e8c8bcce60c98f0f682a6ed6368f0cb
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -304,7 +304,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.mdm.sh \
     init.mi.btmac.sh \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qti.dcvs.sh
