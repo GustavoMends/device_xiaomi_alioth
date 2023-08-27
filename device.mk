a6eac85396f949883f2f19cf144e08163247d0c4
Gustavo Mendes <gusttavo.me@outlook.com>
2023-08-27 18:46:10 -0300

    alioth: Import in-tree media performance configs
    Change-Id: I609c2bae59dd682cee4471db3c0c5a8f7c2f5681

--- device.mk
+++ device.mk
@@ -316,6 +316,10 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor
 
+# Media (Configs)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
