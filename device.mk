c9c08f9c726002b827290cdbfece10c74d9458c5
Gustavo Mendes <gusttavo.me@outlook.com>
2023-09-11 13:39:42 -0300

    alioth: ship MIUI camera
    Change-Id: Ib0f9d8b871a61007233620e8d254871e6b610e06

--- device.mk
+++ device.mk
@@ -198,6 +198,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Camera (MIUI)
+$(call inherit-product-if-exists, vendor/xiaomi/alioth-miuicamera/products/miuicamera.mk)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
