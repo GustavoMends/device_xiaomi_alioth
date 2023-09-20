314c13c404338da805a8eca374493f1797b03cf8
Gustavo Mendes <gusttavo.me@outlook.com>
2023-09-19 21:00:10 -0300

    alioth: Update blobs from MIUI Global Stable V14.0.8.0.TKHMIXM
    Change-Id: Ied019348e05042c2aeecd50d6d93194bdc4624cb

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -18,7 +18,7 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/alioth:13/TKQ1.220829.002/V14.0.3.0.TKHINXM:user/release-keys",
+    .build_fingerprint = "Mi/aliothin/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -32,7 +32,7 @@ static const variant_info_t alioth_global_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_eea/alioth:13/TKQ1.220829.002/V14.0.9.0.TKHEUXM:user/release-keys",
+    .build_fingerprint = "POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHMIXM:user/release-keys",
 
     .nfc = true,
 };
