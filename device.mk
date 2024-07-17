20cc246aa250fe1d1f06eb5ad8b69a731df9b796
someone5678 <nemui3353@gmail.com>
2024-07-17 12:13:53 +0000

    alioth: Build dummy libldacBT_bco
    This target is never existed and Bluetooth apex module also doesn't actually use it. But module still opening the library. Which cause log spam.
    
    a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:135 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I7b5b9c4169ffa18ef82f5657216f32537a28a4d5
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -138,6 +138,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libldacBT_bco
+
 # Camera
 $(call soong_config_set,camera,override_format_from_reserved,true)
 
