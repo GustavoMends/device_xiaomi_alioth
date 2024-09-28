bc0379cb999b1f41fd8fd756677fb358c198911d
Gustavo Mendes <gusttavo.me@outlook.com>
2024-09-28 15:07:42 -0300

    alioth: Rework libinit - Based on aospa alioth - https://github.com/AOSPA/android_device_xiaomi_alioth/blob/uvite/init/init_alioth.cpp.
    Change-Id: I622db9393ee323d04a3f3382eda5883c2aca14c4
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,8 +79,8 @@ TARGET_USES_ION := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_alioth
+TARGET_RECOVERY_DEVICE_MODULES := libinit_alioth
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
