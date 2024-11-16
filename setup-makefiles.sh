28b904aee59a16129ae51529d0ab74a1f2a27177
Gustavo Mendes <gusttavo.me@outlook.com>
2024-11-16 20:54:22 -0300

    fixup!alioth: Add vendor soong imports to extract
    Change-Id: I1888e54e210c62ad4d7fd373463d32ed421ac66c
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,6 +25,7 @@ source "${HELPER}"
 
 function vendor_imports() {
     cat <<EOF >>"$1"
+		"device/xiaomi/alioth",
 		"hardware/qcom-caf/common/libqti-perfd-client",
 		"hardware/qcom-caf/sm8250",
 		"hardware/qcom-caf/wlan",
