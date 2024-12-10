d7f1c8fb1f170e4a533aab37766b0994531629a7
LuK1337 <priv.luk@gmail.com>
2024-12-10 23:07:45 +0100

    alioth: Enable elfchecks for camera libs
    Reference: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8
    Change-Id: I18b0b07bf3a435dd449106653e3ca458b4f07b69
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- extract-files.py
+++ extract-files.py
@@ -51,6 +51,29 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('gettid: 1'),
     'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    (
+         'vendor/lib64/libalAILDC.so',
+         'vendor/lib64/libalLDC.so',
+         'vendor/lib64/libalhLDC.so',
+     ): blob_fixup()
+         .clear_symbol_version('AHardwareBuffer_allocate')
+         .clear_symbol_version('AHardwareBuffer_describe')
+         .clear_symbol_version('AHardwareBuffer_lock')
+         .clear_symbol_version('AHardwareBuffer_release')
+         .clear_symbol_version('AHardwareBuffer_unlock'),
+     (
+         'vendor/lib64/libMIAIHDRhvx_interface.so',
+         'vendor/lib64/libarcsoft_hdrplus_hvx_stub.so',
+         'vendor/lib64/libarcsoft_super_night_raw.so',
+         'vendor/lib64/libmialgo_rfs.so',
+     ): blob_fixup()
+         .clear_symbol_version('remote_handle64_close')
+         .clear_symbol_version('remote_handle64_invoke')
+         .clear_symbol_version('remote_handle64_open')
+         .clear_symbol_version('remote_handle_close')
+         .clear_symbol_version('remote_handle_invoke')
+         .clear_symbol_version('remote_handle_open')
+         .clear_symbol_version('remote_register_buf_attr'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/libril-qc-hal-qmi.so': blob_fixup()
