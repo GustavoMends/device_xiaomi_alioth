5991e311af37934b48e8b82f62dc7a3c9a14d1bf
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    alioth: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
     WifiOverlayM2012K11AI
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
