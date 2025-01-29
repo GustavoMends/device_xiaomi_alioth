66fdaf5dcae2798d7287f645bb85ac6b58df4568
Sugakesshaa <sugakesshaa@pixelos.net>
2025-01-30 01:47:10 +0800

    alioth: Disable Quick Tap
    Change-Id: I128dee996b08b1aaed09fd2ea36a76c67a7690e1

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -14,6 +14,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Device specific
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
+TARGET_SUPPORTS_QUICK_TAP := false
 
 # Pixel stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
