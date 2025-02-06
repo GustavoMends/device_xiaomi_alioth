38e0e1f0f828f1438494cfdd8b3b33cefa915b76
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    alioth: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device.mk
+++ device.mk
@@ -60,6 +60,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
