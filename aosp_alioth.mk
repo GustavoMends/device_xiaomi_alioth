c01001735d150b87ecb3d30c4c02a05ae74fb760
Gustavo Mendes <gusttavo.me@outlook.com>
2025-02-14 09:54:50 -0300

    alioth: Move inherit device makefile to the top
    Change-Id: I7754e524dc42b4e0c997428f03eaf2544650cf2e
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
 # Inherit some common PixelOs stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
@@ -22,9 +25,6 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
 
 # Device identifier
 PRODUCT_NAME := aosp_alioth
