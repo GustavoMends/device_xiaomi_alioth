3def0ec501a357737f07fada75f799b18776c22d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-04-01 18:10:52 +0200

    alioth: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,9 +175,10 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += alioth
