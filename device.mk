a16b93f5b3971352513da1c5b1727eb800ab51e6
Gustavo Mendes <gusttavo.me@outlook.com>
2025-05-14 22:25:48 -0300

    alioth: Stop building Lineage Health
    Change-Id: Ibc770499df0bcece598e8d23ac6a1c98f3de1638
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -220,12 +220,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
-# Lineage Health
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
