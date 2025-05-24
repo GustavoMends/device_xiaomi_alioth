3e06a0b1ad3921ca81b7c8b00603652f1bc737d8
Gustavo Mendes <gusttavo.me@outlook.com>
2025-05-24 10:19:59 -0300

    alioth: Inherit extra stuff
    Change-Id: I83182e07afae6bfa4d3c195c50dabc475a85cb08
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some goodies stuff
+$(call inherit-product, vendor/xiaomi/alioth-extra/config.mk)
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
