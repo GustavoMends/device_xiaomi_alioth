b844e69f7eb7cd06c422504c203a9eba213f7afe
Gustavo Mendes <gusttavo.me@outlook.com>
2025-05-30 22:22:48 -0300

    alioth: Move to Lineage IR HAL
    Change-Id: Ic1f67f15713ca04d1585cac84700a4727b5cc49e
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -157,7 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.example
+    android.hardware.ir-service.lineage
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
