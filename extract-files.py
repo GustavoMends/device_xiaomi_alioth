029c8ee93a20a34c20d4a187c26b8cdfc3ff11ae
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    alioth: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- extract-files.py
+++ extract-files.py
@@ -81,6 +81,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('ro.product.vendor.device', 'ro.vendor.radio.midevice'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
+    'vendor/lib/hw/audio.primary.kona.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
