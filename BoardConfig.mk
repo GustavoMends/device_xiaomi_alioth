2aa667e5fb7a311e929d1a8c4784790b73820a6a
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    alioth: Cleanup more flags
    These are gone after bp conversion for display and media
    
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,9 +88,6 @@ TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-l
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
 TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 
-# Media
-TARGET_USES_ION := true
-
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth|aliothin
 
