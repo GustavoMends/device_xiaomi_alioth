855e393d44edc7f4edeec4dbbeaecd5feb017b5e
Gustavo Mendes <gusttavo.me@outlook.com>
2026-02-17 11:59:59 -0300

    alioth: parts: Checkout to LOS 23.1
    - Choose only the thermal profiles impl.
    - Run google-java-format and clean up a bit while we're at it.
    
    HEAD at:
    - https://github.com/LineageOS/android_device_xiaomi_sm8250-common/commit/8d96cca60847b4b8c2e2847c4194a7e43afc07d6.
    
    Change-Id: Ic350438bb49714fb9caaf8bc0d40b86d1d099d8e
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

---
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.lineageos.settings.thermal.ThermalUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        ThermalUtils.startService(context);
+    }
+}
