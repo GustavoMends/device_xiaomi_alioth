26261f481948c0efe2a737c307ab049de765809a
Gustavo Mendes <gusttavo.me@outlook.com>
2026-02-17 11:59:59 -0300

    alioth: parts: Checkout to LOS 23.1
    - Choose only the thermal profiles impl.
    - Run google-java-format and clean up a bit while we're at it.
    
    HEAD at:
    - https://github.com/LineageOS/android_device_xiaomi_sm8250-common/commit/8d96cca60847b4b8c2e2847c4194a7e43afc07d6.
    
    Change-Id: Ic350438bb49714fb9caaf8bc0d40b86d1d099d8e
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

---
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.thermal;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_THERMAL = "thermal";
+    private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(
+                        com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                        new ThermalSettingsFragment(),
+                        TAG_THERMAL)
+                .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}
